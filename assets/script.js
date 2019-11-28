718da102f37ab218913d055d6fc2d98ff4fe0b1f
SEVERINECP <sevpicot@gmail.com>
2019-11-28 13:15:27 +0100

    JSPartie1Exo5

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+function division(){
+  var number1 = document.getElementById('number1').value;
+  var number2 = document.getElementById('number2').value;
+  var result = number1%number2;
+  alert(result);
+}
