db804a0aa3ec883e7e8e8993850438957735e6e7
SEVERINECP <sevpicot@gmail.com>
2019-11-28 15:02:03 +0100

    JSPartie1Exo5Corrigé

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
+// declaration de la fonction division nous permettant d'afficher le reste de la division
 function division(){
+//Déclaration des variables qui vont récuperer les valeurs des champs de saisie
   var number1 = document.getElementById('number1').value;
   var number2 = document.getElementById('number2').value;
-  var result = number1%number2;
-  alert(result);
+  if (isNaN(number1) == true || isNaN(number2)){
+    alert('erreur: ceci n\'est pas un nombre');
+  }else{
+    if (number2 != 0) {
+      //Déclaration d'une variable contenant le reste de la division du Nombre1 et du Nombre2
+      var result = number1 % number2;
+      //Affichage du résultat
+      //Math.round permet d'arrondir à l'entier le plus proche.
+      alert(Math.round(result));
+    }else{
+      alert('erreur: division par 0 impossible');
+    }
+  }
 }
